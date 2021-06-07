e0b818d6268ce046aeef90642820b0d1424bdbc9
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-06-07 18:41:42 -0500

    1.4 course information step4

--- src/App.js
+++ src/App.js
@@ -22,24 +22,28 @@ const Total = (props) => {
 
 export default function App() {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }
+  ];
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
+      <Content parts={parts} />
 
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Total
+        total={parts[0].exercises + parts[0].exercises + parts[0].exercises}
+      />
     </div>
   );
 }
