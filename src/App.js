5feccb409b48af4ae7a6de8351efaa02f852c620
Santiago Alvarado <34078372+saperez17@users.noreply.github.com>
2021-06-07 18:40:10 -0500

    1.3 course information step3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
 import "./styles.css";
 
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.parts.map((item, idx) => (
+        <p>
+          {item.name} {item.exercises}{" "}
+        </p>
+      ))}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises {props.total}</p>;
+};
+
 export default function App() {
-  return <div className="App">Hi</div>;
+  const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  };
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]} />
+
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  );
 }
